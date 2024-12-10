696fdbc2e73d47af4d5d2ff1aa4e5d72015c6383
MinnieLee <97314459+MinnieLee@users.noreply.github.com>
2024-12-09 23:58:26 -0500

    !

--- scripts.js
+++ scripts.js
@@ -1,20 +1,115 @@
 const images = document.querySelectorAll('.image-container');
 
 images.forEach((image) => {
-  image.addEventListener('mouseover', () => {
-    images.forEach((img) => {
-      if (img !== image) {
-        img.style.opacity = '0.2';
-      }
+    image.addEventListener('mouseover', () => {
+        images.forEach((img) => {
+            if (img !== image) {
+                img.style.opacity = '0.2';
+            }
+        });
     });
-  });
 
-  image.addEventListener('mouseout', () => {
-    images.forEach((img) => {
-      img.style.opacity = '1';
+    image.addEventListener('mouseout', () => {
+        images.forEach((img) => {
+            img.style.opacity = '1';
+        });
     });
-  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
